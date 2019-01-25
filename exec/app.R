77cf90a435ac277670d95df689a43526db03940e
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2019-01-25 00:28:15 +0000

    abandoned clearing output and neatened up app.R

--- exec/app.R
+++ exec/app.R
@@ -4,7 +4,6 @@ library(MIGTranscriptomeExplorer)
 library(gridExtra)
 library(RSQLite)
 library(dplyr)
-library(shinyjs)
 
 # Define UI for MIGTranscriptomeExplorer app
 
@@ -22,7 +21,6 @@ ui <- pageWithSidebar(
 	h2("Explore gene expression across datasets"),
 	h4("Available datasets"),
         actionButton("show.datasets", "show datasets"),
-	actionButton("reset", "Clear"),
 
 	h4("Search for gene in database"),
 	textInput("gene", label="Gene:", value = ""),
@@ -51,12 +49,12 @@ ui <- pageWithSidebar(
 	),
 
     # Main panel for displaying outputs
-    mainPanel(div(id = "alloutputs",
-              dataTableOutput("dataset.table"),
+    mainPanel(dataTableOutput("dataset.table"),
               plotOutput("gene.expression"),
 	      dataTableOutput("significant.results"),
 	      plotOutput("PCA"),
-	      plotOutput("MA")))
+	      plotOutput("MA")
+	      )
 )
 
 db <- system.file("data/csvdb", package="MIGTranscriptomeExplorer")
@@ -65,21 +63,16 @@ conn <- connect(db=db)
 # Define server logic
 server <- function(input, output) {
 
-    observeEvent(input$reset, {
-    hide("dataset.table")
-    })
-
     ######################
     # displaying datasets
     ######################
     data <- eventReactive(input$show.datasets,{
-
-    df <- showDatasets(conn)
-    df
+        df <- showDatasets(conn)
+        df
     })
 
     output$dataset.table <- renderDataTable({
-    data()
+        data()
     })
 
     #####################
@@ -87,23 +80,23 @@ server <- function(input, output) {
     #####################
     expression <- eventReactive(input$gene.search, {
 
-    statement <- 'SELECT dataset FROM reference'
-    datasets <- dbGetQuery(conn, statement)$dataset
+        statement <- 'SELECT dataset FROM reference'
+        datasets <- dbGetQuery(conn, statement)$dataset
 
-    grobs.list <- list()
-    for (i in 1:length(datasets)){
-        dataset <- datasets[i]
-        expression <- getExpression(conn, dataset, input$gene) 
-	if (nrow(expression) == 0){next}
-	metadata <- getMetadata(conn, dataset)
-        p <- plotGeneOfInterest(dataset, expression, metadata, variable="treatment")
-        grobs.list[[i]] <- p
-    }
-    grid.arrange(grobs=grobs.list, ncol=length(datasets))
+        grobs.list <- list()
+        for (i in 1:length(datasets)){
+            dataset <- datasets[i]
+            expression <- getExpression(conn, dataset, input$gene) 
+	    if (nrow(expression) == 0){next}
+	    metadata <- getMetadata(conn, dataset)
+            p <- plotGeneOfInterest(dataset, expression, metadata, variable="treatment")
+            grobs.list[[i]] <- p
+        }
+        grid.arrange(grobs=grobs.list, ncol=length(datasets))
     })
 
     output$gene.expression <- renderPlot({
-    expression()
+        expression()
     })
 
     #####################
@@ -111,71 +104,66 @@ server <- function(input, output) {
     #####################
     significant <- eventReactive(input$significant, {
 
-    statement <- 'SELECT dataset FROM reference'
-    datasets <- dbGetQuery(conn, statement)$dataset
+        statement <- 'SELECT dataset FROM reference'
+        datasets <- dbGetQuery(conn, statement)$dataset
 
-    dfs.list <- list()
-    idx <- 1
-    for (i in 1:length(datasets)){
-        dataset <- datasets[i]
+        dfs.list <- list()
+        idx <- 1
+        for (i in 1:length(datasets)){
+            dataset <- datasets[i]
 
-	# get the contrasts for the dataset
-	contrasts <- getContrasts(conn, dataset)
+	    # get the contrasts for the dataset
+	    contrasts <- getContrasts(conn, dataset)
 
-	# iterate over contrasts
-	for (j in 1:length(contrasts)){
-            contrast <- contrasts[j]
-            significant <- getSignificant(conn, dataset, contrast, input$lfc, input$padj, input$gene)
-	    if (!(is.na(significant)) > 0){
-	        significant$contrast <- contrast
-	        dfs.list[[idx]] <- significant
-		idx <- idx + 1
+	    # iterate over contrasts
+	    for (j in 1:length(contrasts)){
+                contrast <- contrasts[j]
+                significant <- getSignificant(conn, dataset, contrast, input$lfc, input$padj, input$gene)
+	        if (!(is.na(significant)) > 0){
+	            significant$contrast <- contrast
+	            dfs.list[[idx]] <- significant
+		    idx <- idx + 1
 		}
             }
         }
-    df.out <- bind_rows(dfs.list)
+        df.out <- bind_rows(dfs.list)
     })
+
     output$significant.results <- renderDataTable({
-    significant()
+        significant()
     })
 
     #####################
     # plots
     #####################
-
     output$variable <- renderUI({
-    variables <- append("none", getMetadataList(conn, input$choose.dataset))
-    names(variables) <- variables
-    selectInput("variable", "variable", variables)
+        variables <- append("none", getMetadataList(conn, input$choose.dataset))
+        names(variables) <- variables
+        selectInput("variable", "variable", variables)
     })
-    
-
     PCA <- eventReactive(input$PCA, {
-    mat <- getMatrix(conn, input$choose.dataset)
-
-    metadata <- getMetadata(conn, input$choose.dataset)
-    metadata <- sortMetadata(mat, metadata)
-    metadata$none <- "none"
-
-    pc <- runPCA(mat)
-    plotPrincipleComponents(pc, metadata, colourby=input$variable)
+        mat <- getMatrix(conn, input$choose.dataset)
+        metadata <- getMetadata(conn, input$choose.dataset)
+        metadata <- sortMetadata(mat, metadata)
+        metadata$none <- "none"
+        pc <- runPCA(mat)
+        plotPrincipleComponents(pc, metadata, colourby=input$variable)
     })
     output$PCA <- renderPlot({
-    PCA()
+        PCA()
     })
 
     output$ma.contrast <- renderUI({
-    contrasts <- getContrasts(conn, input$choose.dataset)
-    names(contrasts) <- contrasts
-    selectInput("ma.contrast", "contrast", contrasts)
+        contrasts <- getContrasts(conn, input$choose.dataset)
+        names(contrasts) <- contrasts
+        selectInput("ma.contrast", "contrast", contrasts)
     })
-
     MA <- eventReactive(input$MA, {
-    result <- getResultSet(conn, input$choose.dataset, input$ma.contrast)
-    plotMA(result, lfc=input$ma.lfc, title=paste(input$choose.dataset, input$ma.contrast, sep=": "))
+        result <- getResultSet(conn, input$choose.dataset, input$ma.contrast)
+        plotMA(result, lfc=input$ma.lfc, title=paste(input$choose.dataset, input$ma.contrast, sep=": "))
     })
     output$MA <- renderPlot({
-    MA()
+        MA()
     })
 }
 
