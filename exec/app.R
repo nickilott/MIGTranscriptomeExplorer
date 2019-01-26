53b1509cc97a26517edafc06592ed9a6c6d5a551
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2019-01-26 23:27:55 +0000

    testing checkbox and hide

--- exec/app.R
+++ exec/app.R
@@ -97,7 +97,7 @@ server <- function(input, output) {
     # displaying datasets
     ######################
     observeEvent(input$show.datasest, {
-        shinyjs::toggle("dataset.table")
+        if input£show.dataset shinyjs::show("dataset.table") else shinyjs::hide("dataet.table")
     })
     #data <- eventReactive(input$show.datasets,{
     #    df <- showDatasets(conn)
