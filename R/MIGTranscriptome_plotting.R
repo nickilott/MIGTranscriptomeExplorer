cb54ef714f4de6bd4be14fcd34f61a27c037a560
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2019-01-28 16:01:04 +0000

    debugging plotting no data

--- R/MIGTranscriptome_plotting.R
+++ R/MIGTranscriptome_plotting.R
@@ -20,7 +20,8 @@ plotGeneOfInterest <- function(dataset, mat, metadata, variable="treatment"){
 
     # plot for if there's no data
     if (nrow(mat) == 0){
-        plot1 <- ggplot(data.frame(x=1, y=1, text="No data"), aes(x=x, y=y, text=text)) + geom_text(label=text)
+        df <- data.frame(x=1, y=1, text.output="No data")
+        plot1 <- ggplot(df, aes(x=x, y=y)) + geom_text(label=text.output)
 	plot2 <- plot1 + theme(panel.background=element_rect(fill="white", colour="white"))
 	plot3 <- plot2 + theme(panel.grid.major=element_line(colour="white"))
         plot4 <- plot3 + theme(panel.grid.minor=element_line(colour="white"))
